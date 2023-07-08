89219843d573c6a5e8da7169678f2852c2184d66
Nandi Balakeswara <putunandi123@gmail.com>
2023-07-08 14:31:29 +0800

    Menyelesaikan fitur searc gejala dan mood

--- application/models/mSearchGejala.php
+++ application/models/mSearchGejala.php
@@ -2,19 +2,119 @@
 class mSearchGejala extends CI_Model
 {
     
-    public function searchDataGejala($keyword, $KodeUser){
-        $this->db->select("DATE(tbgejala.Waktu) AS GejalaWaktu, DATE(tbmakan.Waktu) AS MakanWaktu, tbmakan.NamaMakan, COUNT(tbmakan.NamaMakan) AS jumlah");
-        $this->db->from('tbgejala');
-        $this->db->join('tbmakan', 'tbgejala.KodeUser = tbmakan.KodeUser',"left");
-        $this->db->where("tbgejala.Gejala LIKE '%$keyword%'");
-        $this->db->where('tbgejala.KodeUser',$KodeUser);
-        $this->db->where('tbmakan.Waktu >= DATE_SUB(DATE(tbgejala.Waktu), INTERVAL 3 DAY)');
-        $this->db->group_by('tbmakan.NamaMakan');
-        $this->db->order_by('tbgejala.Waktu', 'desc');
-        $this->db->order_by('COUNT(tbmakan.NamaMakan)', 'desc');
-        $query = $this->db->get();
-        $hasil = $query->result_array();
-        return $hasil;
+   function searchDataGejala($keyword, $KodeUser){
+        $hasil1['gejala']=$this->db->select('*')
+            ->from('tbgejala')
+            ->where('KodeUser', $KodeUser)
+            ->where('Gejala',$keyword)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+
+        $hasil1['hitung'] =$this->db->select('NamaMakan, COUNT(KodeMakan) AS jumlah')
+            ->from('tbmakan')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbgejala WHERE Gejala = '$keyword' AND KodeUser = $KodeUser)", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbgejala WHERE Gejala = '$keyword' AND KodeUser = $KodeUser)", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbgejala WHERE Gejala = '$keyword' AND KodeUser = $KodeUser)", NULL, FALSE)
+            ->group_by('NamaMakan')
+            ->order_by('jumlah', 'DESC')
+            ->order_by('NamaMakan')
+            ->get()
+            ->result_array();
+
+        $hasil1['makan'] =$this->db->select('*')
+            ->from('tbmakan')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbgejala WHERE Gejala = '$keyword' AND KodeUser = $KodeUser )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbgejala WHERE Gejala = '$keyword' AND KodeUser = $KodeUser )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbgejala WHERE Gejala = '$keyword' AND KodeUser = $KodeUser )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+        return $hasil1;
+    }
+    function lihat($KodeGejala, $KodeUser){
+
+        $hasil2['makan'] =$this->db->select('*')
+            ->from('tbmakan')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+        $hasil2['tidur'] =$this->db->select('*')
+            ->from('tbtidur')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(WaktuMulai) IN (SELECT DATE(Waktu) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->or_where("DATE(WaktuMulai) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->or_where("DATE(WaktuMulai) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->order_by('WaktuMulai', 'DESC')
+            ->get()
+            ->result_array();  
+        $hasil2['mood'] =$this->db->select('*')
+            ->from('tbmood')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();  
+        $hasil2['bab'] =$this->db->select('*')
+            ->from('tbbab')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbgejala WHERE KodeGejala=$KodeGejala )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+       return $hasil2;
+    }
+    function ditail($KodeMakan, $KodeUser) {
+        $hasil2['makan'] =$this->db->select('*')
+            ->from('tbmakan')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) = (SELECT DATE(Waktu) FROM tbmakan WHERE KodeMakan=$KodeMakan )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+        $hasil2['tidur'] =$this->db->select('*')
+            ->from('tbtidur')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(WaktuMulai) = (SELECT DATE(Waktu) FROM tbmakan WHERE KodeMakan=$KodeMakan )", NULL, FALSE) 
+            ->order_by('WaktuMulai', 'DESC')
+            ->get()
+            ->result_array();  
+        $hasil2['mood'] =$this->db->select('*')
+            ->from('tbmood')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) = (SELECT DATE(Waktu) FROM tbmakan WHERE KodeMakan=$KodeMakan )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();  
+        $hasil2['bab'] =$this->db->select('*')
+            ->from('tbbab')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) = (SELECT DATE(Waktu) FROM tbmakan WHERE KodeMakan=$KodeMakan )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+       return $hasil2;
+    }
+    function lihatJum($NamaMakan,$Gejala,$KodeUser){
+        $hasil3 =$this->db->select('*')
+            ->from('tbmakan')
+            ->where('NamaMakan', $NamaMakan)
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(waktu) >= ALL (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbgejala WHERE Gejala = '$Gejala' AND KodeUser = $KodeUser)", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+        return $hasil3;
     }
 }
 ?>
--- application/models/mSearchMood.php
+++ application/models/mSearchMood.php
@@ -1,20 +1,121 @@
 <?php 
 class mSearchMood extends CI_Model
 {
-    
-    public function searchDataMood($keyword, $KodeUser){
-        $this->db->select("DATE(tbmood.Waktu) AS MoodWaktu, DATE(tbmakan.Waktu) AS MakanWaktu, tbmakan.NamaMakan, COUNT(tbmakan.NamaMakan) AS jumlah");
-        $this->db->from('tbmood');
-        $this->db->join('tbmakan', 'tbmood.KodeUser = tbmakan.KodeUser',"left");
-        $this->db->where("tbmood.Mood LIKE '%$keyword%'");
-        $this->db->where('tbmood.KodeUser',$KodeUser);
-        $this->db->where('tbmakan.Waktu >= DATE_SUB(DATE(tbmood.Waktu), INTERVAL 3 DAY)');
-        $this->db->group_by('tbmakan.NamaMakan');
-        $this->db->order_by('tbmood.Waktu', 'desc');
-        $this->db->order_by('COUNT(tbmakan.NamaMakan)', 'desc');
-        $query = $this->db->get();
-        $hasil = $query->result_array();
-        return $hasil;
+    function searchDataMood($keyword, $KodeUser){
+        $hasil1['mood']=$this->db->select('*')
+            ->from('tbmood')
+            ->where('KodeUser', $KodeUser)
+            ->where('Mood',$keyword)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+
+        $hasil1['hitung'] =$this->db->select('NamaMakan, COUNT(KodeMakan) AS jumlah')
+            ->from('tbmakan')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbmood WHERE Mood = '$keyword' AND KodeUser = $KodeUser)", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbmood WHERE Mood = '$keyword' AND KodeUser = $KodeUser)", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbmood WHERE Mood = '$keyword' AND KodeUser = $KodeUser)", NULL, FALSE)
+            ->group_by('NamaMakan')
+            ->order_by('jumlah', 'DESC')
+            ->order_by('NamaMakan')
+            ->get()
+            ->result_array();
+
+        $hasil1['makan'] =$this->db->select('*')
+            ->from('tbmakan')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbmood WHERE Mood = '$keyword' AND KodeUser = $KodeUser )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbmood WHERE Mood = '$keyword' AND KodeUser = $KodeUser )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbmood WHERE Mood = '$keyword' AND KodeUser = $KodeUser )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+        return $hasil1;
+    }
+
+    function lihat($KodeMood, $KodeUser){
+
+        $hasil2['makan'] =$this->db->select('*')
+            ->from('tbmakan')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+        $hasil2['tidur'] =$this->db->select('*')
+            ->from('tbtidur')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(WaktuMulai) IN (SELECT DATE(Waktu) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->or_where("DATE(WaktuMulai) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->or_where("DATE(WaktuMulai) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->order_by('WaktuMulai', 'DESC')
+            ->get()
+            ->result_array();  
+        $hasil2['gejala'] =$this->db->select('*')
+            ->from('tbgejala')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();  
+        $hasil2['bab'] =$this->db->select('*')
+            ->from('tbbab')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) IN (SELECT DATE(Waktu) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 1 DAY)) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->or_where("DATE(waktu) IN (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbmood WHERE KodeMood=$KodeMood )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+       return $hasil2;
+    }
+
+    function ditail($KodeMakan, $KodeUser) {
+        $hasil2['makan'] =$this->db->select('*')
+            ->from('tbmakan')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) = (SELECT DATE(Waktu) FROM tbmakan WHERE KodeMakan=$KodeMakan )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+        $hasil2['tidur'] =$this->db->select('*')
+            ->from('tbtidur')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(WaktuMulai) = (SELECT DATE(Waktu) FROM tbmakan WHERE KodeMakan=$KodeMakan )", NULL, FALSE) 
+            ->order_by('WaktuMulai', 'DESC')
+            ->get()
+            ->result_array();  
+        $hasil2['gejala'] =$this->db->select('*')
+            ->from('tbgejala')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) = (SELECT DATE(Waktu) FROM tbmakan WHERE KodeMakan=$KodeMakan )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();  
+        $hasil2['bab'] =$this->db->select('*')
+            ->from('tbbab')
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(Waktu) = (SELECT DATE(Waktu) FROM tbmakan WHERE KodeMakan=$KodeMakan )", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+       return $hasil2;
+    }
+    function lihatJum($NamaMakan,$Mood,$KodeUser){
+        $hasil3 =$this->db->select('*')
+            ->from('tbmakan')
+            ->where('NamaMakan', $NamaMakan)
+            ->where('KodeUser', $KodeUser)
+            ->where("DATE(waktu) >= ALL (SELECT DATE(DATE_SUB(waktu, INTERVAL 2 DAY)) FROM tbmood WHERE Mood = '$Mood' AND KodeUser = $KodeUser)", NULL, FALSE)
+            ->order_by('Waktu', 'DESC')
+            ->get()
+            ->result_array();
+        return $hasil3;
     }
 }
 ?>
